5062eb0acbd8cc9ffa012ff5a4efdc8112ebb30e
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-04 01:06:22 +0530

    feat: launch v0.7 studio workflow shell

--- app/src/main/java/com/framebynavin/app/MainActivity.kt
+++ app/src/main/java/com/framebynavin/app/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.framebynavin.app.reminders.ReminderNotifications
-import com.framebynavin.app.ui.FrameByNavinV061App
+import com.framebynavin.app.ui.FrameByNavinV07App
 import com.framebynavin.app.ui.theme.FrameByNavinTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FrameByNavinTheme {
-                FrameByNavinV061App()
+                FrameByNavinV07App()
             }
         }
     }
