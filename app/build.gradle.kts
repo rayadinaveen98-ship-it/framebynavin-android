f5c43d2ae37003a468eb2955a4a8267ecd5c64a2
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-03 23:44:15 +0530

    chore: version v0.6 smart escalation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.framebynavin.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.5.1-alarm-precision"
+        versionCode = 8
+        versionName = "0.6.0-smart-escalation"
     }
 
     compileOptions {
