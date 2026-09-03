f196ff96b94451aa689f1c0f8137c018be32557d
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-04 02:08:44 +0530

    chore: bump app to v0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.framebynavin.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.8.0-weekly-engine"
+        versionCode = 13
+        versionName = "0.9.0-release-vault"
     }
 
     compileOptions {
