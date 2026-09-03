b81ce1ae574f1af39dc6c39f31bd2ebc592be6f0
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-04 03:43:25 +0530

    fix: provide supported Studio navigation icon

---
+++ app/src/main/java/androidx/compose/material/icons/outlined/MovieEditCompat.kt
@@ -0,0 +1,7 @@
+package androidx.compose.material.icons.outlined
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.ui.graphics.vector.ImageVector
+
+val Icons.Outlined.MovieEdit: ImageVector
+    get() = Icons.Outlined.VideoCameraBack
