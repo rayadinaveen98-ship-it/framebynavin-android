98a32e4da3d145ae2129799cb10bf420441d4156
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-04 01:42:42 +0530

    feat: launch v0.8 weekly engine shell

--- app/src/main/java/com/framebynavin/app/MainActivity.kt
+++ app/src/main/java/com/framebynavin/app/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.framebynavin.app.reminders.ReminderNotifications
-import com.framebynavin.app.ui.FrameByNavinV07App
+import com.framebynavin.app.ui.FrameByNavinV08App
 import com.framebynavin.app.ui.theme.FrameByNavinTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FrameByNavinTheme {
-                FrameByNavinV07App()
+                FrameByNavinV08App()
             }
         }
     }
