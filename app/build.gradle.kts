b5ea5af573df5ec6dd562094d4fbd7a4cdc31710
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-04 11:30:28 +0530

    build: version v1.2 widget capture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.framebynavin.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "1.1.0-youtube-insights"
+        versionCode = 20
+        versionName = "1.2.0-widget-capture"
     }
 
     compileOptions {
