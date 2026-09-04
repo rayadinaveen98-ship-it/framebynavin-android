4cebc2c96d83c585f64d7b2497c26030e500bff3
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-04 22:04:57 +0530

    feat(v1.7): add creator automation widget routes

--- app/src/main/java/com/framebynavin/app/widget/CreatorWidgetContract.kt
+++ app/src/main/java/com/framebynavin/app/widget/CreatorWidgetContract.kt
@@ -5,6 +5,11 @@ object CreatorWidgetContract {
     const val ACTION_OPEN_STUDIO = "com.framebynavin.app.widget.OPEN_STUDIO"
     const val ACTION_NEW_PROJECT = "com.framebynavin.app.widget.NEW_PROJECT"
     const val ACTION_RELEASE_DAY = "com.framebynavin.app.widget.RELEASE_DAY"
+    const val ACTION_DAILY_BRIEF = "com.framebynavin.app.widget.DAILY_BRIEF"
+    const val ACTION_CONTENT_CALENDAR = "com.framebynavin.app.widget.CONTENT_CALENDAR"
+    const val ACTION_IDEA_VAULT = "com.framebynavin.app.widget.IDEA_VAULT"
+    const val ACTION_OPEN_INSIGHTS = "com.framebynavin.app.widget.OPEN_INSIGHTS"
+    const val ACTION_AUTOMATION_CENTER = "com.framebynavin.app.widget.AUTOMATION_CENTER"
     const val EXTRA_TASK_ID = "widget_task_id"
 }
 
