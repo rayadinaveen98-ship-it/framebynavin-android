47809c361aca9a58394e6b369aefee12434d2187
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-07 00:13:36 +0530

    feat(piano): add core data module

---
+++ piano-ci/core/data/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.pianostudio.core.data"
+    compileSdk = 36
+    defaultConfig { minSdk = 26 }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("androidx.datastore:datastore-preferences:1.2.1")
+}
