65cc88c6d89f8ac3f2edd40d3066cc8c6c6bb3e3
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-11 21:49:40 +0530

    build(v115): bump Brain explainability version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.framebynavin.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 114
-        versionName = "2.0.0-alpha1.4d-creator-brain-recommendations"
+        versionCode = 115
+        versionName = "2.0.0-alpha1.4e-brain-explainability-controls"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
