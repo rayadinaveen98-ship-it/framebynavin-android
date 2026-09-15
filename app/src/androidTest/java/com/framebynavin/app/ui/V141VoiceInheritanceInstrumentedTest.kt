aeb3f25ae070a96ba67e75704f387ab6b90a2111
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-15 23:38:05 +0530

    test(v141): fix Compose node matcher import

--- app/src/androidTest/java/com/framebynavin/app/ui/V141VoiceInheritanceInstrumentedTest.kt
+++ app/src/androidTest/java/com/framebynavin/app/ui/V141VoiceInheritanceInstrumentedTest.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasSetTextAction
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
