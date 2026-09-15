15fa965dd86be185a6792e6ed8e13cafe5d5a9c5
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-15 19:34:23 +0530

    chore(v138): bump Backlot visual identity build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ android {
         applicationId = "com.framebynavin.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 137
-        versionName = "2.0.0-rc10-visual-personalization"
+        versionCode = 138
+        versionName = "2.0.0-rc11-visual-identity"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
