182df74910e7c6a49133d2f6a714f77542407966
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-15 16:12:15 +0530

    feat(v134): configure Compose desktop packaging

---
+++ desktop/build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm") version "2.4.20"
+    id("org.jetbrains.compose") version "1.12.0"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.20"
+}
+
+group = "com.backlot"
+version = "0.1.0"
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    implementation(compose.material3)
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+compose.desktop {
+    application {
+        mainClass = "com.backlot.desktop.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Msi, TargetFormat.Exe)
+            packageName = "Backlot"
+            packageVersion = "0.1.0"
+            description = "Backlot creator workspace"
+            vendor = "Backlot"
+
+            windows {
+                upgradeUuid = "641dd83e-96cf-4e82-8c0b-70e8cf341340"
+            }
+        }
+    }
+}
